9eeca4a33fa32337a8f5ca79f2fe5c9714043ad5
Shan Khalam <dev.shankhalam@gmail.com>
2023-10-05 02:11:28 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from tkinter import *
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def timer():
+    count_down(10)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    canvas.itemconfig(counter_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro App")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(window, text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_png = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_png)
+counter_text = canvas.create_text(102, 130, text="00:00", fill="white",
+                                  font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="START", command=timer)
+start_button.grid(column=0, row=3)
+
+reset_button = Button(text="RESET")
+reset_button.grid(column=2, row=3)
+
+tick_label = Label(text="✔️", font=(FONT_NAME, 20), bg=YELLOW, fg=GREEN)
+tick_label.grid(column=1, row=4)
+
+window.mainloop()
